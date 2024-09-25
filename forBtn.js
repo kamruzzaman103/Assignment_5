4faa9de13cf0bd285528bb69a9d2095d55c1fc1c
Kamruzzaman Tutul <kamruzzaman103>
2024-09-25 11:38:30 +0600

    added

--- forBtn.js
+++ forBtn.js
@@ -1,7 +1,4 @@
-    // Get both buttons
-    // var button1 = document.getElementById('button1');
-    // var button2 = document.getElementById('button2');
-    document.getElementById('donationBtn').addEventListener('click', function() {
+  document.getElementById('donationBtn').addEventListener('click', function() {
         selectButton('donationBtn');
     });
 
@@ -9,18 +6,14 @@
         selectButton('historyBtn');
     });
     function selectButton(buttonId) {
-        // Reset both buttons to default state (unselected)
         donationBtn.classList.remove('bg-[#b4f461]', 'tex-black');
         donationBtn.classList.add('bg-white', 'text-gray-500');
         
         historyBtn.classList.remove('bg-[#b4f461]', 'tex-black');
         historyBtn.classList.add('bg-white', 'text-gray-500');
-
-        // Change the clicked button to selected state
         var selectedButton = document.getElementById(buttonId);
         selectedButton.classList.remove('bg-white', 'text-gray-500');
         selectedButton.classList.add('bg-[#b4f461]', 'tex-black');
     }
 
-    // Add event listeners for both buttons
     
